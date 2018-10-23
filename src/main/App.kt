d3b5db981d6afbacd4b3657791708b07ac563db5
Douglas Fletcher <fletchy_d@hotmail.com>
2018-10-23 22:56:40 +0200

    adding readme

--- src/main/App.kt
+++ src/main/App.kt
@@ -10,7 +10,6 @@ class Main {
     fun main(args: Array<String>) {
         val mtext = MysteriousText("./data/20181021_125852.jpg")
         println(mtext.getImgText())
-        //System.out.println(app.getImgText("C:\\Users\\User\\Pictures\\img.png"))
     }
 
 }
