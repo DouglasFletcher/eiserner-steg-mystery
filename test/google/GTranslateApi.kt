4ea0f92303c8594a5c641d057cf98a3fbe5d85f9
Douglas Fletcher <fletchy_d@hotmail.com>
2018-10-23 23:33:34 +0200

    add dummy tests

---
+++ test/google/GTranslateApi.kt
@@ -0,0 +1,10 @@
+package utility
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GTranslateApiTest {
+    @Test fun testOk() {
+        assertEquals("OK", "OK")
+    }
+}
---
+++ test/utility/MysteriousTextTest.kt
@@ -0,0 +1,10 @@
+package utility
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MysteriousTextTest {
+    @Test fun testOk() {
+        assertEquals("OK", "OK")
+    }
+}
