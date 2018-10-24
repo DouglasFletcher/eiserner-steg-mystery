43ed14d0831338c4313e298642049b69a248d7e3
Douglas Fletcher <fletchy_d@hotmail.com>
2018-10-24 22:02:36 +0200

    change to google vision api

--- src/main/App.kt
+++ src/main/App.kt
@@ -1,14 +1,12 @@
 package main
 
-import main.utility.MysteriousText
+import main.utility.GoogleVisionWrapper
 
 /**
  * lets (go) kotlin ...
  */
 fun main(args : Array<String>) {
-
-    val mystery = MysteriousText("images/20181024_010309.jpg")
-    mystery.getImgText()
-    mystery.saveImgText()
+    val gvWrapper = GoogleVisionWrapper("images/20181024_010309.jpg")
+    //gvWrapper.detectText()
 }
 
