8747cd3fe23eebee80a26116f556b9a2b486797d
Douglas Fletcher <fletchy_d@hotmail.com>
2018-10-24 22:05:20 +0200

    update test class

--- test/utility/GoogleVisionWrapperTest.kt
+++ test/utility/GoogleVisionWrapperTest.kt
@@ -3,7 +3,7 @@ package utility
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class GTranslateApiTest {
+class GoogleVisionWrapperTest {
     @Test fun testOk() {
         assertEquals("OK", "OK")
     }
